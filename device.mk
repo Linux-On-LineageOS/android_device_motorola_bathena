8327b1293d76522e9949dfae14406391f2088841
Archie Meng <archiemeng@protonmail.com>
2025-11-03 20:29:34 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -84,3 +84,4 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/bathena/bathena-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
